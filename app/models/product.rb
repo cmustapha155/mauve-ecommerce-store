46f964978d4b487ba7f80eb69a8d198b7f55e10c
Caleb Mustapha <cmustapha@academic.rrc.ca>
2025-03-31 09:49:00 -0500

    Add activeadmin, run install generator, create admin user, register with Product model, resolve admin/products page error

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,8 @@
 class Product < ApplicationRecord
+  def self.ransackable_attributes(auth_object = nil)
+    [ "created_at", "description", "id", "id_value", "name", "price", "updated_at" ]
+  end
+
   validates :name, presence: true
   validates :price, presence: true
 end
