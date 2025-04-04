63b479bceefc80b2a00f7fc69d2fb14d135b567b
Caleb Mustapha <cmustapha@academic.rrc.ca>
2025-04-04 17:41:31 -0500

    Add Product and Category association

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,9 +1,17 @@
 class Category < ApplicationRecord
   before_save :generate_slug
 
+  # allowlist attributes as searchable for activeadmin
   def self.ransackable_attributes(auth_object = nil)
     [ "created_at", "id", "name", "slug", "updated_at" ]
   end
+
+  # allowlist association as searchable for activeadmin (allow access to associated product info from here)
+  def self.ransackable_associations(auth_object = nil)
+    ["products"] # plural
+  end
+
+  has_many :products
 
   validates :name, presence: true, uniqueness: { case_sensitive: false }
   validates :slug, uniqueness: { case_sensitive: false }
--- app/models/product.rb
+++ app/models/product.rb
@@ -1,15 +1,22 @@
 class Product < ApplicationRecord
   before_save :generate_slug
 
+  # allowlist attributes as searchable for activeadmin
   def self.ransackable_attributes(auth_object = nil)
-    [ "created_at", "description", "id", "id_value", "name", "price", "updated_at", "slug" ]
+    [ "created_at", "description", "id", "id_value", "name", "price", "updated_at", "slug", "category_id" ]
   end
+
+  # allowlist associations as searchable for activeadmin (allow access to associated category info from here)
+  def self.ransackable_associations(auth_object = nil)
+    ["category"] # singular
+  end
+
+  belongs_to :category
 
   # validation required for each property, AND by property type (except strings)
   validates :name, presence: true, uniqueness: { case_sensitive: false }
   validates :price, presence: true, numericality: { greater_than_or_equal_to: 0 }
   validates :description, presence: true
-
   validates :slug, uniqueness: { case_sensitive: false }
 
   private def generate_slug
