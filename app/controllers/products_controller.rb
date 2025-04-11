9d5f9519cdec6dc4d36c4f3bb485dc8697fe0ff9
Caleb Mustapha <cmustapha@academic.rrc.ca>
2025-04-10 19:05:09 -0500

    Add header and nav partials to products' index and show pages and categories' show pages

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,9 +1,11 @@
 class ProductsController < ApplicationController
   def index
     @products = Product.all
+    @categories = Category.all
   end
 
   def show
     @product = Product.find(params[:id])
+    @categories = Category.all
   end
 end
