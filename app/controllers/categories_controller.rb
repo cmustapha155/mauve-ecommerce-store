f0c7cd103a3fa3b56a3ddd165fc4fd16f5e782a8
Caleb Mustapha <cmustapha@academic.rrc.ca>
2025-04-23 02:11:56 -0500

    Add product listings by category on category pages

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,6 +1,9 @@
 class CategoriesController < ApplicationController
   def show
     @categories = Category.all
-    @products = Product.all
+    @category = Category.find(params[:id])
+
+    # the products, as a category knows it, is limited to the products with that category id
+    @products = Product.where(category_id: @category)
   end
 end
