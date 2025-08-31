ff54866502f4caafda54e82ef379397da05aca96
Caleb Mustapha <cmustapha@academic.rrc.ca>
2025-08-31 18:11:41 -0500

    Set default status for new orders

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2025_08_31_225408) do
+ActiveRecord::Schema[7.2].define(version: 2025_08_31_231019) do
   create_table "active_admin_comments", force: :cascade do |t|
     t.string "namespace"
     t.text "body"
@@ -94,7 +94,7 @@ ActiveRecord::Schema[7.2].define(version: 2025_08_31_225408) do
 
   create_table "orders", force: :cascade do |t|
     t.integer "customer_id"
-    t.string "status"
+    t.string "status", default: "new"
     t.decimal "subtotal"
     t.decimal "provincial_sales_tax"
     t.decimal "general_sales_tax"
