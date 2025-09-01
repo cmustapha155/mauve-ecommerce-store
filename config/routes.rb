f8f6e26672f39e9ebb87e80e9005b5ab958921d2
Caleb Mustapha <cmustapha@academic.rrc.ca>
2025-08-31 23:41:52 -0500

    Implement User Sign In/Out

--- config/routes.rb
+++ config/routes.rb
@@ -28,7 +28,6 @@ Rails.application.routes.draw do
   get "search" => "search#search_results", as: "search_results"
 
   # Enable devise routes for customers, which generates sign up/sign in tools automatically
-  # devise_for :customers
 
   resources :orders, only: [ :create, :show ]
 
